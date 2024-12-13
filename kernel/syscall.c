7cb9b7a2e54a946ab5acfe1e607ba840043b22d7
Dr_Astor <rider.astol@gmail.com>
2024-12-13 21:44:07 +0800

    fix_finish_lab1_challenge1

--- kernel/syscall.c
+++ kernel/syscall.c
@@ -40,7 +40,7 @@ ssize_t sys_user_exit(uint64 code) {
 //stop when meet func 'main'
 int func_print_name(uint64 ret_addr){
   // sprint("%d\n", sym_count);
-  for(int i = 0;i <= sym_count;i ++){
+  for(int i = 0;i < sym_count;i ++){
     if(ret_addr >= elf_symbols[i].st_value && ret_addr < elf_symbols[i].st_value + elf_symbols[i].st_size){
       sprint("%s\n", sym_names[i]);
       if(strcmp(sym_names[i], "main") == 0) return 0;
@@ -54,7 +54,7 @@ ssize_t sys_print_backtrace(uint64 depth){
   uint64 trace_sp = current->trapframe->regs.sp + 32;
   uint64 trace_ra = trace_sp + 8;
   uint64 tracer = 0;
-  for(; tracer <= depth; tracer ++){
+  for(; tracer < depth; tracer ++){
     if(func_print_name(*(uint64 *) trace_ra) == 0) return tracer;
     trace_ra += 16;
   }
